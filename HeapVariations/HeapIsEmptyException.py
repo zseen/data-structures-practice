369cf75d0686eb0dfe5fc387c9fd834adcee0b31
zseen <43151120+zseen@users.noreply.github.com>
2019-02-23 20:10:49 +0000

    Refactor - rename variables

--- HeapVariations/HeapIsEmptyException.py
+++ HeapVariations/HeapIsEmptyException.py
@@ -1,8 +1,2 @@
 class HeapIsEmptyException(IndexError):
-    # def __init__(self, message):
-    #     self.message = message
-    #
-    # def __str__(self):
-    #     return self.message
-
     pass
--- HeapVariations/RuntimeTestOfHeaps.py
+++ HeapVariations/RuntimeTestOfHeaps.py
@@ -6,23 +6,27 @@ import random
 import time
 
 
-INITIAL_ELEMENTS_LIST = [random.randrange(0, 100) for _ in range(10)]
-TEST_RANGE = 10
-NUM_DIGITS_AFTER_DECIMAL_POINT = 4
+INITIAL_ELEMENTS_NUM: int = 10
+INITIAL_ELEMENTS_VALUES_RANGE: int = 100
+ADD_ELEMENTS_REPEAT_RANGE: int = 15000
+REMOVE_ELEMENTS_REPEAT_RANGE: int = 1500
+
+NUM_TEST_ITERATIONS: int = 10
+NUM_DIGITS_AFTER_DECIMAL_POINT: int = 4
 
 
-def modifyHeap(heap, action):
+def modifyHeap(heap, action: str):
     if action == "add":
-        for repeat in range(15000):
+        for repeat in range(ADD_ELEMENTS_REPEAT_RANGE):
             heap.add(random.randrange(0, 100000))
     elif action == "remove":
-        for repeat in range(1500):
+        for repeat in range(REMOVE_ELEMENTS_REPEAT_RANGE):
             heap.getAndRemoveSmallest()
 
 
 def benchmarkHeapImplementation(heapImplementation):
     runTimes = []
-    for test in range(TEST_RANGE):
+    for test in range(NUM_TEST_ITERATIONS):
         start = time.clock()
 
         executeQueries(heapImplementation)
@@ -30,7 +34,7 @@ def benchmarkHeapImplementation(heapImplementation):
         end = time.clock()
         runTimes.append(end - start)
 
-    print(round(sum(runTimes) / TEST_RANGE, NUM_DIGITS_AFTER_DECIMAL_POINT))
+    print(round(sum(runTimes) / NUM_TEST_ITERATIONS, NUM_DIGITS_AFTER_DECIMAL_POINT))
 
 
 def executeQueries(h):
@@ -40,7 +44,7 @@ def executeQueries(h):
 
 
 def main():
-    benchmarkHeapImplementation(SimpleArrayBasedHeap(INITIAL_ELEMENTS_LIST))
+    benchmarkHeapImplementation(SimpleArrayBasedHeap([random.randrange(INITIAL_ELEMENTS_VALUES_RANGE) for _ in range(INITIAL_ELEMENTS_NUM)]))
 
 
 if __name__ == '__main__':
