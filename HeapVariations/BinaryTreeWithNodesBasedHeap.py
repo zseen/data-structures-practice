dd2e723db77d2e5515d48df10f1052cbe8742a9c
zseen <43151120+zseen@users.noreply.github.com>
2019-02-24 21:00:35 +0000

    Add method findParentOfMissingChild() to find the parent of the new node to insert

--- HeapVariations/BinaryTreeWithNodesBasedHeap.py
+++ HeapVariations/BinaryTreeWithNodesBasedHeap.py
@@ -1,6 +1,6 @@
 import IHeap
 from typing import List
-import time
+from queue import deque
 
 
 class Node:
@@ -8,80 +8,59 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+        self.parent = None
+
 
 
 class Heap(IHeap.IHeap):
     def __init__(self):
         self.root = None
 
-    def addElementToHeap(self, value):
-        if self.root is None:
-            self.root = Node(value)
-            #print("root:", self.root.value)
-        elif value < self.root.value:
-            oldNode = self.root
-            self.root = Node(value)
-            #print("root:", self.root.value)
-            self._insert(oldNode.value, self.root)
-        else:
-            self._insert(value, self.root)
 
-    def _insert(self, value, currentNode):
-        #print("value:", value)
-        #print("currNodeVal: ", currentNode.value)
-        if currentNode.value < value:
-            if currentNode.left is None:
-                currentNode.left = Node(value)
-            elif currentNode.right is None:
-                currentNode.right = Node(value)
+    def add(self, element: int):
+
+
+    def findParentOfMissingChild(self):
+        missingChild = Node(0)
+        missingChild.parent = None
+        nodesToVisit = deque()
+
+        nodesToVisit.appendleft(self.root)
+        visitedNodes = set()
+
+        while nodesToVisit:
+            currentNode = nodesToVisit.pop()
+            visitedNodes.add(currentNode)
+
+            #print("currentNode: ", currentNode.value)
+
+            if currentNode.left and currentNode.right:
+                nodesToVisit.appendleft(currentNode.left)
+                nodesToVisit.appendleft(currentNode.right)
             else:
-                self._insert(value, currentNode.left)
-        else:
-            oldNode = currentNode
-            currentNode = Node(value)
-            self._insert(oldNode.value, currentNode.right)
-
-
-
-    def removeAndReturnSmallestElement(self):
-        oldRoot = self.root
-
-        if not oldRoot.left:
-            if oldRoot.right:
-                self.root = oldRoot.right
-                return oldRoot.value
-        elif not oldRoot.right:
-            if oldRoot.left:
-                self.root = oldRoot.left
-                return oldRoot.value
-        else:
-            if oldRoot.left.value < oldRoot.right.value:
-                self.root = oldRoot.left
-            else:
-                self.root = oldRoot.right
-        return oldRoot.value
-
-
-
+                missingChild.parent = currentNode
+                return missingChild.parent
 
 
 def main():
     h = Heap()
 
-    h.addElementToHeap(3)
-    h.addElementToHeap(2)
-    h.addElementToHeap(4)
-    h.addElementToHeap(1)
-    h.addElementToHeap(5)
+    h.root = Node(2)
+    h.root.left = Node(3)
+    #h.root.left.parent = h.root
+    h.root.right = Node(4)
 
-    h.addElementToHeap(6)
-    print("smallest: ", h.removeAndReturnSmallestElement())
-    print("smallest: ", h.removeAndReturnSmallestElement())
-    print("smallest: ", h.removeAndReturnSmallestElement())
-    print("smallest: ", h.removeAndReturnSmallestElement())
-    h.addElementToHeap(7)
-    #h.removeAndReturnSmallestElement()
-    #print("new root", h.root.value)
+    h.root.left.left = Node(5)
+    #h.root.right.parent = h.root
+    h.root.left.right = Node(6)
+
+    h.root.right.left = Node(7)
+    h.root.right.right = Node(8)
+    print(h.findParentOfMissingChild().value)
+
+
+
+    #print(h.root.value)
 
 
 if __name__ == '__main__':
