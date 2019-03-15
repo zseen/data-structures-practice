51e2ad8ddf09b67b5b00e5c33543a355170fee96
zseen <43151120+zseen@users.noreply.github.com>
2019-03-15 15:39:43 +0000

    Insert nodes in the tree properly when they are in order from 1-7

--- HeapVariations/BTWNBH.py
+++ HeapVariations/BTWNBH.py
@@ -15,26 +15,44 @@ class Node:
 
 
 class BinaryTreeWithNodesBasedHeap(IHeap):
-    def __init__(self, initialElements: List):
+    def __init__(self):
         self.root = None
         self.currentSize = 0
-        self.currentNode = None
+        self.currentNode = self.root
         self.currentLayerCapacity = 0
         self.isLayerFull = False
 
-    def findInsertionPoint(self, newNode: Node):
+    def add(self, element: int):
+        newNode = Node(element)
         if not self.root:
             self.root = newNode
             self.isLayerFull = True
+            self.currentSize += 1
+
+        else:
             self.currentNode = self.root
-        self.currentSize += 1
-
-        while self.currentNode.left:
-            self.goLeft()
-
+            self.currentSize += 1
+            newNodePositionInBinary = self.getBinaryValueOfNodePosition()
+            print(newNodePositionInBinary)
+            for char in newNodePositionInBinary[3:]:
+                if char == "0":
+                    if self.currentNode.left:
+                        self.goLeft()
+                    else:
+                        self.currentNode.left = newNode
+                        newNode.parent = self.currentNode
+                elif char == "1":
+                    if self.currentNode.right:
+                        self.goRight()
+                    else:
+                        self.currentNode.right = newNode
+                        newNode.parent = self.currentNode
 
     def goLeft(self):
         self.currentNode = self.currentNode.left
+
+    def goRight(self):
+        self.currentNode = self.currentNode.right
 
 
     def getLayerCapacity(self):
@@ -51,5 +69,59 @@ class BinaryTreeWithNodesBasedHeap(IHeap):
         else:
             return True
 
+    def getNodePositionInLayer(self):
+        nodePosition = self.currentSize - self.currentLayerCapacity
+        return nodePosition
+
+    def getBinaryValueOfNodePosition(self):
+        nodePosition = self.getNodePositionInLayer()
+        binaryNodePosition = bin(nodePosition)
+        return binaryNodePosition
+
+    def printUpToThreeLayersOfTreeWithParents(self) -> None:
+        # if self.isHeapEmpty():
+        #     raise HeapIsEmptyException("Heap empty - cannot print tree")
+
+        print("...")
+        if self.root.right:
+            if self.root.right.right:
+                print("  " + "root.right.right: ", self.root.right.right.value)
+                print("  " + "root.right.right.parent: ", self.root.right.right.parent.value)
+            if self.root.right.left:
+                print("  " + "root.right.left: ", self.root.right.left.value)
+                print("  " + "root.right.left.parent: ", self.root.right.left.parent.value)
+
+            print(" " + "root.right: ", self.root.right.value)
+            print(" " + "root.right.parent: ", self.root.right.parent.value)
+
+        print("root: ", self.root.value)
+
+        if self.root.left:
+            print(" " + "root.left: ", self.root.left.value)
+            print(" " + "root.left.parent: ", self.root.left.parent.value)
+            if self.root.left.right:
+                print("  " + "root.left.right: ", self.root.left.right.value)
+                print("  " + "root.left.right.parent: ", self.root.left.right.parent.value)
+            if self.root.left.left:
+                print("  " + "root.left.left: ", self.root.left.left.value)
+                print("  " + "root.left.left.parent: ", self.root.left.left.parent.value)
+        print("...")
+
+def main():
+    h = BinaryTreeWithNodesBasedHeap()
+    h.add(1)
+    h.add(2)
+    h.add(3)
+    h.add(4)
+    h.add(5)
+    h.add(6)
+    h.add(7)
+
+    h.printUpToThreeLayersOfTreeWithParents()
+
+
+
+if __name__ == '__main__':
+    main()
 
 
